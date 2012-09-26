055c54f05f976e5be46a55e0eea38cb26ae189d4
Irfan Nurhakim <myfunareaa@gmail.com>
2012-09-27 00:25:56 +0700

    add file reader

--- src/com/tokenizer/util/FileRecursiveReader.java
+++ src/com/tokenizer/util/FileRecursiveReader.java
@@ -5,7 +5,6 @@
 package com.tokenizer.util;
 
 import java.io.File;
-import java.util.AbstractList;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -16,6 +15,8 @@ import java.util.List;
 public class FileRecursiveReader {
     
     public File file;
+    public List<File> list = new ArrayList<File>();                
+
     
     public FileRecursiveReader(String rootFolder){
         this.file = new File(rootFolder);
@@ -24,19 +25,22 @@ public class FileRecursiveReader {
     }
     
     
-    private List<File> getListDirectory(File file){
-        List<File> list = new ArrayList<File>();                
-        
-        if(this.isDirectory(file)){
+    private List<File> getListDirectory(File file){    
+        if(this.isDirectory(file) && !file.getName().equalsIgnoreCase("all_documents")){
             for(int i=0;i<file.listFiles().length;i++){
                 this.getListDirectory(file.listFiles()[i]);
             }
         } else {
-            System.out.println(file.getAbsolutePath());
+            list.add(file);
         }
         
         return list;
     }
+
+    public List<File> getList() {
+        return list;
+    }
+    
     
     private Boolean isDirectory(File file){
         return (file.isDirectory());
--- src/tokenizer/Tokenizer.java
+++ src/tokenizer/Tokenizer.java
@@ -6,8 +6,12 @@ package tokenizer;
 
 import com.tokenizer.util.FileRecursiveReader;
 import java.io.File;
-import java.util.ArrayList;
-import java.util.List;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -20,10 +24,23 @@ public class Tokenizer {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        
-        FileRecursiveReader frs = new FileRecursiveReader("/Users/hadipratama/Documents/Kuliah/Search_Engine_Tech/enron_mail_20110402/maildir/symes-k/confirms");
 
+        FileRecursiveReader frs = new FileRecursiveReader("/Users/hadipratama/Documents/Kuliah/Search_Engine_Tech/enron_mail_20110402/maildir/symes-k/");
+
+        for (int i = 0; i < frs.getList().size(); i++) {
+            
+            System.out.println(frs.getList().get(i).getName());
+           /* StringBuilder text = new StringBuilder();
+
+            try {
+                Scanner scanner = new Scanner(frs.getList().get(i));
+                while (scanner.hasNextLine()) {
+                    text.append(scanner.nextLine());
+                }
+            } catch (FileNotFoundException ex) {
+                Logger.getLogger(Tokenizer.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            System.out.println(text);*/
+        }
     }
-    
-    
 }
