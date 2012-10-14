6e2a0cb45f048f1e28f21d947bd4033b0be6c4f3
Irfan Nurhakim <myfunareaa@gmail.com>
2012-10-14 13:25:57 +0700

    code beautification

--- src/com/tokenizer/util/FileReader.java
+++ src/com/tokenizer/util/FileReader.java
@@ -23,15 +23,14 @@ public class FileReader implements Callable {
     private Path path;
     private int count;
 
-    public FileReader(){
+    public FileReader() {
     }
-    
-    
+
     public FileReader(Path path, int count) {
         this.path = path;
         this.count = count;
     }
-   
+
     public void setCaller(FileWalker fileWalker) {
         this.fileWalker = fileWalker;
     }
@@ -45,22 +44,21 @@ public class FileReader implements Callable {
 
         String line = Files.readAllLines(this.path, StandardCharsets.UTF_8).toString().toLowerCase().replaceAll("x-to|x-from", "");
         //System.out.println(line);
-     
+
         /*
          * raw -> array 0 head, array 1 tail
          */
         String[] raw = line.split("date: ", 2);
-        
-        String [] date = raw[1].split("from: ",2);
-        HashMap <String,Integer> dateMap = dateTokenizer.getListDate(date[0]);    
-        
-        synchronized(BigConcurentHashMap.dateConcurentMap )
-        {
-        BigConcurentHashMap.mergeBigHashMap(BigConcurentHashMap.dateConcurentMap, dateMap);
+
+        String[] date = raw[1].split("from: ", 2);
+        HashMap<String, Integer> dateMap = dateTokenizer.getListDate(date[0]);
+
+        synchronized (BigConcurentHashMap.dateConcurentMap) {
+            BigConcurentHashMap.mergeBigHashMap(BigConcurentHashMap.dateConcurentMap, dateMap);
         }
-        
+
         //System.out.println(dateMap);
-        
+
         if (date.length == 1) {
             date[1] = "";
         }
@@ -70,16 +68,15 @@ public class FileReader implements Callable {
             from = date[1].split("to: ", 2);
         } else {
             from = date;
-        }        
-        
-        HashMap <String,Integer> fromMap = FromTokenizer.getListFrom(from[0].replaceAll(", ", " "));
-        
-        synchronized(BigConcurentHashMap.fromConcurentMap )
-        {
-        BigConcurentHashMap.mergeBigHashMap(BigConcurentHashMap.fromConcurentMap, fromMap);
+        }
+
+        HashMap<String, Integer> fromMap = FromTokenizer.getListFrom(from[0].replaceAll(", ", " "));
+
+        synchronized (BigConcurentHashMap.fromConcurentMap) {
+            BigConcurentHashMap.mergeBigHashMap(BigConcurentHashMap.fromConcurentMap, fromMap);
         }
         //System.out.println(fromMap);
-        
+
         if (from.length == 1) {
             from[1] = "";
         }
@@ -90,14 +87,13 @@ public class FileReader implements Callable {
         } else {
             to = from;
         }
-        
-        HashMap <String,Integer> toMap = toTokenizer.getListTo(to[0]);
-         
-        synchronized(BigConcurentHashMap.toConcurentMap )
-        {
-        BigConcurentHashMap.mergeBigHashMap(BigConcurentHashMap.toConcurentMap, toMap);
+
+        HashMap<String, Integer> toMap = toTokenizer.getListTo(to[0]);
+
+        synchronized (BigConcurentHashMap.toConcurentMap) {
+            BigConcurentHashMap.mergeBigHashMap(BigConcurentHashMap.toConcurentMap, toMap);
         }
-        
+
         if (to.length == 1) {
             to[1] = "";
         }
@@ -108,17 +104,16 @@ public class FileReader implements Callable {
         } else {
             subject = to;
         }
-        
-        HashMap <String,Integer> subjectMap = subject_bodyTokenizer.getListTerm(subject[0]);
-        
-        synchronized(BigConcurentHashMap.subjectConcurentMap )
-        {
-        BigConcurentHashMap.mergeBigHashMap(BigConcurentHashMap.subjectConcurentMap, subjectMap);
+
+        HashMap<String, Integer> subjectMap = subject_bodyTokenizer.getListTerm(subject[0]);
+
+        synchronized (BigConcurentHashMap.subjectConcurentMap) {
+            BigConcurentHashMap.mergeBigHashMap(BigConcurentHashMap.subjectConcurentMap, subjectMap);
         }
 
-        HashMap <String,Integer> bodyMap = new HashMap<String, Integer>();
-        
-        
+        HashMap<String, Integer> bodyMap = new HashMap<String, Integer>();
+
+
         if (subject.length == 1) {
             subject[1] = "";
         }
@@ -132,24 +127,22 @@ public class FileReader implements Callable {
 
         if (body.length == 1) {
             body[1] = "";
-        }        
-        
-        bodyMap= subject_bodyTokenizer.getListTerm(body[1]);
-        
-        synchronized(BigConcurentHashMap.bodyConcurentMap )
-        {
-        BigConcurentHashMap.mergeBigHashMap(BigConcurentHashMap.bodyConcurentMap, bodyMap);
         }
-       
-        HashMap <String,Integer> allFieldMap = AllFieldTokenizer.allFieldTermList(dateMap, toMap, fromMap, subjectMap, bodyMap);
-       
-        synchronized(BigConcurentHashMap.allConcurentMap )
-        {
-        BigConcurentHashMap.mergeBigHashMap(BigConcurentHashMap.allConcurentMap, allFieldMap);
+
+        bodyMap = subject_bodyTokenizer.getListTerm(body[1]);
+
+        synchronized (BigConcurentHashMap.bodyConcurentMap) {
+            BigConcurentHashMap.mergeBigHashMap(BigConcurentHashMap.bodyConcurentMap, bodyMap);
+        }
+
+        HashMap<String, Integer> allFieldMap = AllFieldTokenizer.allFieldTermList(dateMap, toMap, fromMap, subjectMap, bodyMap);
+
+        synchronized (BigConcurentHashMap.allConcurentMap) {
+            BigConcurentHashMap.mergeBigHashMap(BigConcurentHashMap.allConcurentMap, allFieldMap);
         }
         //System.out.println(allFieldMap);
-        
- 
+
+
         //System.out.println(path.toString());
         fileWalker.callback(dateMap, fromMap, toMap, subjectMap, bodyMap, allFieldMap, count);
         return true;
